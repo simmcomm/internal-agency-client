f3e7cfe55ca1c20d008af10218f98813466fe25d
Ivan Pepelko <ivan.pepelko@gmail.com>
2024-02-09 14:20:17 +0100

    Initial commit

---
+++ frid-store.ts
@@ -0,0 +1,38 @@
+export type FridStore = {
+  /**
+   * @param invalidateStale Only relevant if store supports expiring
+   */
+  getFrid(invalidateStale?: boolean): string | undefined;
+  setFrid(frid: string, expire?: number): void;
+}
+
+let inMemoryFrid: string;
+export const createInMemoryFridStore = (): FridStore => {
+  return {
+    getFrid(): string | undefined {
+      return inMemoryFrid;
+    },
+    setFrid(frid: string) {
+      inMemoryFrid = frid;
+    },
+  };
+};
+
+/**
+ * Creates LocalStorage (default) or SessionStorage frid store
+ */
+export const createWebStorageFridStore = (
+  storage: Storage = window.localStorage,
+  key: string = 'frid',
+): FridStore => {
+  return {
+    getFrid(invalidateStale?: boolean): string | undefined {
+      const frid = storage.getItem(key);
+      if (frid !== null) return frid;
+      return undefined;
+    },
+    setFrid(frid: string, expire?: number) {
+      storage.setItem(key, frid);
+    },
+  };
+};
---
+++ index.ts
@@ -0,0 +1,199 @@
+import { fetch as crossFetch, type Response } from 'cross-fetch';
+import { createInMemoryFridStore, type FridStore } from './frid-store.js';
+
+export type { FridStore } from './frid-store.js';
+
+const ANTIFRAUD_SCRIPT_ID = `antifraud-${Date.now()}`;
+
+type EvinaNotifyFunction = (event: Event, cb?: () => void) => void;
+
+function evinaNotifyInWindow(window: Window): window is Window & { evina_notify: EvinaNotifyFunction } {
+  return 'evina_notify' in window && typeof window.evina_notify === 'function';
+}
+
+type Action = 'submitmsisdn' | 'event' | 'check' | 'antifraud';
+
+type _SubmitMsisdnResponse = { message: string; };
+
+export type SubmitMsisdnOkResponse = _SubmitMsisdnResponse & {
+  status: 'ok';
+  sms_to: string;
+  sms_body: string;
+  frid: string;
+};
+
+export type SubmitMsisdnErrorResponse = _SubmitMsisdnResponse & {
+  status: 'error';
+  frid?: string;
+};
+
+type SubmitMsisdnResponse = SubmitMsisdnOkResponse | SubmitMsisdnErrorResponse;
+
+export type SaveEventResponse = {
+  status: 'ok' | 'error';
+  message: string;
+  frid?: string;
+};
+
+export type CheckSubscriptionResponse = {
+  status: 'ok';
+  message: string;
+  active: boolean;
+};
+
+export type InternalAgencyClient = {
+  saveEvent(event: string, data?: unknown): Promise<SaveEventResponse>;
+  submitMsisdn(msisdn: string): Promise<SubmitMsisdnResponse>;
+  checkSubscription(msisdn?: string): Promise<CheckSubscriptionResponse>;
+  loadAntifraud(selector: string, options?: { tag?: string, observerTarget?: Element }): Promise<void>;
+};
+
+class ResponseError extends Error {
+  constructor(
+    public readonly response: Response,
+    public readonly data: unknown,
+  ) {
+    super('internal-agency: request failed (see data field)');
+  }
+}
+
+export function createInternalAgencyClient(parameters: {
+  campaignId: string;
+  serviceId: string;
+  apiEndpoint?: string;
+  fridStore?: FridStore;
+  fetch?: typeof window.fetch;
+}): Readonly<InternalAgencyClient> {
+  const { campaignId, serviceId } = parameters;
+  const apiEndpoint = parameters.apiEndpoint ?? 'https://agency-api.flowly.com/internal/';
+  const fridStore = parameters.fridStore ?? createInMemoryFridStore();
+  const fetch = parameters.fetch ?? crossFetch;
+
+  function createApiUrl(action: Action, requestData: Record<string, unknown>): URL {
+    const url = new URL(apiEndpoint);
+
+    const parameters: Record<string, string> = {
+      action,
+      campaignid: campaignId,
+      serviceid: serviceId,
+      ...requestData,
+    };
+
+    if (typeof window !== 'undefined' && 'location' in window) {
+      parameters.original_uri = window.location.toString();
+    }
+
+    url.search = new URLSearchParams(
+      Object.fromEntries(
+        Object.entries(parameters).filter(([, value]) => Boolean(value)),
+      ),
+    ).toString();
+
+    return url;
+  }
+
+  async function doFetch(
+    method: 'GET' | 'POST',
+    action: Action,
+    requestData = {},
+    requestOptions = {},
+  ) {
+    const url = createApiUrl(action, requestData);
+    const frid = fridStore.getFrid();
+    if (frid) {
+      url.searchParams.set('frid', frid);
+    }
+
+    const response = await fetch(url, {
+      method,
+      ...requestOptions,
+    });
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new ResponseError(response, data);
+    }
+
+    if ('frid' in data) {
+      fridStore.setFrid(data.frid);
+    }
+
+    return data;
+  }
+
+  const submitMsisdn = (msisdn: string): Promise<SubmitMsisdnResponse> => {
+    return doFetch('GET', 'submitmsisdn', { msisdn });
+  };
+
+  const saveEvent = (event: string, data?: unknown): Promise<SaveEventResponse> => {
+    return doFetch('POST', 'event', { event }, { body: JSON.stringify(data) });
+  };
+
+  const checkSubscription = (msisdn = undefined): Promise<CheckSubscriptionResponse> => {
+    const params: Partial<Record<'msisdn' | 'frid', string>> = {};
+    if (msisdn) {
+      params.msisdn = msisdn;
+    } else {
+      params.frid = fridStore.getFrid();
+    }
+    return doFetch('GET', 'check', params);
+  };
+
+  const loadAntifraud = async (
+    selector: string,
+    options: { tag?: string, observerTarget?: Element },
+  ): Promise<void> => {
+    if (!(typeof window !== 'undefined' && window === globalThis)) {
+      throw new Error('internal-agency-client: calling loadAntifraud() makes sense only in browser environment');
+    }
+
+    const tag = options?.tag ?? 'optin2';
+    const observerTarget = options?.observerTarget ?? undefined;
+
+    const { document } = window;
+
+    if (document.getElementById(ANTIFRAUD_SCRIPT_ID) !== null && evinaNotifyInWindow(window)) {
+      return Promise.resolve();
+    }
+
+    const url = createApiUrl('antifraud', { tag, selector, frid: fridStore.getFrid() });
+
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.id = ANTIFRAUD_SCRIPT_ID;
+      script.src = url.toString();
+
+      script.addEventListener('load', () => {
+        document.dispatchEvent(new Event('DCBProtectRun'));
+        new MutationObserver((records, observer) => {
+          if (records.length === 0) return;
+          records.forEach(record => {
+            record.target.addEventListener('click', (ev) => {
+              if (evinaNotifyInWindow(window)) {
+                window.evina_notify(ev, () => { /* noop */ });
+              }
+            }, { once: true });
+          });
+          observer.disconnect();
+        }).observe(
+          observerTarget ?? document.querySelector(selector) ?? document.body,
+          {
+            attributes: true,
+            subtree: true,
+            attributeFilter: ['data-antifraud'],
+          },
+        );
+        resolve();
+      });
+      script.addEventListener('error', reject);
+      document.body.appendChild(script);
+    });
+  };
+
+  return Object.freeze({
+    submitMsisdn,
+    saveEvent,
+    checkSubscription,
+    loadAntifraud,
+  });
+}
